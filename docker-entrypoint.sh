1ecfecf8948959cb5b444c7751170ca8dd8b4851
alligatorme <alligatorme@gmail.com>
2016-03-29 22:20:12 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,5 @@
 # /code/manage.py migrate --fake-initial
 # /code/manage.py syncdb
 # /code/manage.py createsuperuser --username alligator --email a@a.com
+/code/manage.py migrate
 /code/manage.py runserver 0.0.0.0:8000
